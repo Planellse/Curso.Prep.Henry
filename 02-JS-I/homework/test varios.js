995a12afe5035ae9d182669799db322f729f2377
Esteban Planells <planells_e@yahoo.com.ar>
2021-02-14 00:48:23 -0300

    CP commiteado por planellse a las 1613274503527

---
+++ 02-JS-I/homework/test varios.js
@@ -0,0 +1,13 @@
+var str1;
+var str2;
+var largo1;
+var largo2;
+
+str1 = "palabra1";
+str2 = "palabra2";
+
+largo1 = length(str1);
+largo2 = length(str1);
+
+console.log(largo1);
+console.log(largo2);
--- 02-JS-I/homework/tests/JSI.test.js
+++ 02-JS-I/homework/tests/JSI.test.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 const {
-  nuevaString,
+  nuevaString = "HOLA",
   nuevoNum,
   nuevoBool,
   nuevaResta,
